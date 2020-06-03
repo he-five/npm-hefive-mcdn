d15a42a19d166bb367d20aeac7dc02764e7f080d
simeon-iotsyst <simeon.dobrikov@devcloudbg.com>
2020-06-03 12:56:06 +0300

    * project setup with bells and whistles

--- rollup.config.js
+++ rollup.config.js
@@ -6,31 +6,31 @@ import resolve from 'rollup-plugin-node-resolve'
 import pkg from './package.json'
 
 export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs',
-            exports: 'named',
-            sourcemap: true
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            exports: 'named',
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        external(),
-        resolve(),
-        typescript({
-            rollupCommonJSResolveHack: true,
-            exclude: '**/__tests__/**',
-            clean: true
-        }),
-        commonjs({
-            include: ['node_modules/**']
-        })
-    ]
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      exclude: '**/__tests__/**',
+      clean: true
+    }),
+    commonjs({
+      include: ['node_modules/**']
+    })
+  ]
 }
